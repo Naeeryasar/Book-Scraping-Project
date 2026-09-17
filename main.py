36667bcd36d5e1f02f3ccde229b641077a65061a
Naeeryasar <naeeryasar3486@gmail.com>
2026-09-17 17:28:58 +0600

    Scrapeing pages

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ from urllib.parse import urljoin
 
 header = {"User-Agent": "Mozilla/5.0"}
 all_book = []
+
+for i in range(1, 4):
+    url = f"https://books.toscrape.com/catalogue/page-{i}.html"
+    print(f"Scrapping Page {i}")
+
+    response = requests.get(url, headers=header)
+
+    if response.status_code != 200:
+        print("Page not found")
+        continue
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    books = soup.find_all("article", class_="product_pod")
